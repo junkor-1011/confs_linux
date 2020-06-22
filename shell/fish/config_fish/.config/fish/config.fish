3df6225644bd2fe0ed3e735910ed65622c049849
test-user <test@email.com>
2020-06-22 21:32:09 +0900

    fisher

--- shell/fish/config_fish/.config/fish/config.fish
+++ shell/fish/config_fish/.config/fish/config.fish
@@ -15,6 +15,13 @@ alias ll='ls -alF'
 alias la='ls -AF'
 alias l='ls -CF'
 
+# fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 # local setting
 if test -f $HOME/.config/fish/config_local.fish
     source $HOME/.config/fish/config_local.fish
