a8e614f28bc6203bda1b3ac0426d3c4616a06e5d
test-user <test@email.com>
2020-12-24 22:41:09 +0900

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ for target in ${TARGET_LIST[@]}; do
     echo $target
     /bin/bash ${DEPLOY_SCRIPTS_PATH[$target]} ${DEPLOY_SCRIPTS_OPTS[$target]}
 
-    if [ $DEPLOY_SCRIPTS_SET_DEFAULT != "" ]; then
+    if [ ${DEPLOY_SCRIPTS_SET_DEFAULT[$target]} != "" ]; then
         /bin/bash ${DEPLOY_SCRIPTS_SET_DEFAULT}
     fi
 done
