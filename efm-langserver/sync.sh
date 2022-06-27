fb35f0cd309050ffec01fa7e33895c42a04fa398
junkor-1011 <tomoyukizxy@gmail.com>
2022-06-28 00:55:47 +0900

    feat(efm-langserver): update shfmt setting(tab->space4)

--- efm-langserver/sync.sh
+++ efm-langserver/sync.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 set -eu
 
-SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
+SCRIPT_DIR="$(
+    cd "$(dirname "$0")"
+    pwd
+)"
 cd "${SCRIPT_DIR}"
 
 if [ "$1" = "link" ]; then
     ln -sfv "$(pwd)/config.yaml" "${HOME}/.config/efm-langserver/config.yaml"
 else
-    echo "wrong argument \n"
+    echo "wrong argument"
 fi
