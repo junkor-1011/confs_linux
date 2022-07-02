e7d8a5b2feccbd42c07d95c2f135cc2e5a584bb3
junkor-1011 <tomoyukizxy@gmail.com>
2022-07-02 13:38:29 +0900

    fix(efm-langserver): check ~/.config/efm-langserver

--- efm-langserver/sync.sh
+++ efm-langserver/sync.sh
@@ -7,6 +7,10 @@ SCRIPT_DIR="$(
 )"
 cd "${SCRIPT_DIR}"
 
+if [ ! -d "${HOME}/.config/efm-langserver" ]; then
+    mkdir -p "${HOME}/.config/efm-langserver"
+fi
+
 if [ "$1" = "link" ]; then
     ln -sfv "$(pwd)/config.yaml" "${HOME}/.config/efm-langserver/config.yaml"
 else
